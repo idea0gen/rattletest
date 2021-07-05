959d7bb23df539c03ec13d7c4dc69d01883a498c
Hiresh Dandekar <2361550+firebrick@users.noreply.github.com>
2021-07-05 09:57:32 +0530

    Added code to create,modify,list testcase. Also fixed some additional html templates and some custom css and js

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("", include("projects.urls"), name="project"),
+    path("", include("testcases.urls"), name="testcase"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- rattletest/static/js/project.js
+++ rattletest/static/js/project.js
@@ -1,1 +1,6 @@
 /* Project specific Javascript goes here. */
+
+var form_elements = document.getElementsByClassName("form-control");
+for(var i=0;i<form_elements.length;i++) {
+  form_elements.item(i).classList.add('form-control-sm');
+}
