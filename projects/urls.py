ecf26f6c043ced7f07aaa3c019ae039958cbc1e8
firebrick <2361550+firebrick@users.noreply.github.com>
2022-03-05 20:21:54 +0530

    Updated libraries

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ from projects import views
 urlpatterns = [
     path("project/create/", views.ProjectCreate.as_view(), name="new_project"),
     path(
-        "project/<int:pk>/edit/", views.ProjectUpdateView.as_view(), name="project_edit"
+        "project/<int:project_id>/edit/", views.ProjectUpdateView.as_view(), name="project_edit"
     ),
     path("project/", views.ProjectListView.as_view(), name="projects"),
 ]
