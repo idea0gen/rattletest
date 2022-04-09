95c6089f25a6f6d8cfbcd3b8d7a0fbda908aeb1f
H D <2361550+firebrick@users.noreply.github.com>
2022-04-09 09:33:50 +0530

    Deployment to railway (#24)
    Added Procfile, requirements.txt,runtime.txt

--- config/settings/production.py
+++ config/settings/production.py
@@ -99,7 +99,7 @@ EMAIL_SUBJECT_PREFIX = env(
 # Anymail
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ["anymail"]  # noqa F405
+# INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
