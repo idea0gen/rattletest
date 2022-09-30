efe48fc3e43bb8cc1fbb4402c7b68d885bcb79ac
H D <2361550+firebrick@users.noreply.github.com>
2022-09-30 22:54:54 +0530

    Us 32 edit link (#43)
    * Add changes for prod deployment
    
    * Pre-commit fix
    
    * Add additional user profile fields to capture

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,8 +65,6 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "crispy_forms",
     "allauth",
-    # "allauth.account",
-    # "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
     "allauth.socialaccount.providers.gitlab",
     "anymail",
